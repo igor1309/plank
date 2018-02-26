c23001aca62931215eec4bbdceff301064f37c4b
Igor <igor1309@gmail.com>
2018-02-26 16:39:22 +0300

    very second commit - first lost in changing of folder struct

---
+++ Plank App/Plank App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plank App
+//
+//  Created by Igor Malyarov on 25.02.2018.
+//  Copyright © 2018 Igor Malyarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
